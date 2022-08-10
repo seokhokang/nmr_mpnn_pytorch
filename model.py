97e67e85fef578661d08ef9497ded5f2134e11fe
Seokho Kang <39222137+seokhokang@users.noreply.github.com>
2022-08-10 10:04:36 +0900

    Update model.py

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ class nmrMPNN(nn.Module):
         self.predict = nn.Sequential(
             nn.Linear(node_feats * (1 + num_step_message_passing) * 3, hidden_feats), nn.PReLU(), nn.Dropout(prob_dropout),
             nn.Linear(hidden_feats, hidden_feats), nn.PReLU(), nn.Dropout(prob_dropout),
-            nn.Linear(hidden_feats, hidden_feats), nn.PReLU(), 
+            nn.Linear(hidden_feats, hidden_feats), nn.PReLU(), nn.Dropout(prob_dropout),
             nn.Linear(hidden_feats, 1)
         )                           
                                
